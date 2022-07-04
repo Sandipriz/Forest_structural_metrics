3867f7d466bb3c911a794134843e9485a79033db
bearsofthemoss <43791909+bearsofthemoss@users.noreply.github.com>
2022-07-04 14:19:57 -0400

    Updated R script

--- neon_lidar_melnhe/neon_plot_locations.R
+++ neon_lidar_melnhe/neon_plot_locations.R
@@ -3,9 +3,6 @@
 
 library(neonUtilities)
 library(devtools)
-devtools::install_github("NEONScience/NEON-geolocation/geoNEON")  # work with NEON spatial data
-
-
 library(geoNEON)
 
 
@@ -17,13 +14,31 @@ tree <-
     check.size = FALSE
   )
 
-names(tree)
+names(tree) # these are the data sub-files
 tree
+
+head(tree$vst_perplotperyear)
+
+plots<-tree$vst_perplotperyear
+
+table(plots$nlcdClass)
+
+table(plots$eventID, plots$plotID )
+
+lp<-as.data.frame(table(plots$plotID, plots$plotType ))
+
+
+lp$nlcd<-plots$nlcdClass[match(lp$Var1, plots$plotID)]
+lp$easting<-plots$easting[match(lp$Var1, plots$plotID)]
+lp$northing<-plots$northing[match(lp$Var1, plots$plotID)]
+
+head(lp)
+write.csv(lp, file="Bartlett plots.csv")
 
 # you can get locations of veg plots conveniently using getLocTOS
 m <- getLocTOS(data = tree$vst_mappingandtagging,
                dataProd = "vst_mappingandtagging")
-
+m
 # calculate the mean easting and northing for the trees. this gets plot centers (to use for lidar)
 locMean <-
   aggregate(
